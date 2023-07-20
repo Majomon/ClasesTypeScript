6690c7a6755b1056fa3c8b046d186d5ee3656ff4
Majomon <mauri.monzon91@gmail.com>
2023-07-20 14:16:43 -0300

    Ejercicios

--- Modulo 1/module01.ts
+++ Modulo 1/module01.ts
@@ -1,4 +1,4 @@
-function addNumbers(x:number, y:number) {
-    return x + y;
-  }
-  console.log(addNumbers(3, 6));
+function AddNumbers(num1: number, num2: number) {
+  return num1 + num2;
+}
+console.log(AddNumbers(3, 6));
